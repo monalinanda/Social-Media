1e6cd145c78b6b18b5c5a6e49325036d1147656a
monalinanda <monalinanda95@gmail.com>
2020-08-07 13:37:44 +0530

    Updates

---
+++ precache-manifest.f1886af21a68ecc51b9f9323ed14ca94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88175a250ebe9d4c5b6ae0ff5847022c",
+    "url": "/Social-Media/index.html"
+  },
+  {
+    "revision": "0d6014dc687a639c085c",
+    "url": "/Social-Media/static/css/main.aad6e7c9.chunk.css"
+  },
+  {
+    "revision": "a1547019256e2dd7de78",
+    "url": "/Social-Media/static/js/2.87636757.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Social-Media/static/js/2.87636757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6014dc687a639c085c",
+    "url": "/Social-Media/static/js/main.e542e7f3.chunk.js"
+  },
+  {
+    "revision": "e7d778c6ebdfc1834417",
+    "url": "/Social-Media/static/js/runtime-main.1f176651.js"
+  }
+]);
